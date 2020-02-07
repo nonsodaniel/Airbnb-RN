4226ca8e0faebb46b3ed44d8ef62a634b5d87567
nonsodaniel <nonsodaniel07@gmail.com>
2020-02-07 13:07:33 +0100

    Added search area

--- screens/Explore.js
+++ screens/Explore.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Platform, StatusBar } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
 
 class Explore extends Component {
+    componentWillMount() {
+        this.startHeaderHeight = 80;
+        if (Platform.OS == "android") {
+            this.startHeaderHeight = 100 + StatusBar.currentHeight
+        }
+    }
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Open Up in the Explore place</Text>
-            </View>
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={{ flex: 1 }}>
+                    <View style={{
+                        height: this.startHeaderHeight,
+                        backgroundColor: "white",
+                        borderBottomWidth: 1, borderBottomColor: "#dddddd"
+                    }}>
+                        <View style={{
+                            flexDirection: "row", padding: 10, backgroundColor: "white",
+                            marginHorizontal: 20, shadowOffset: { width: 0, height: 0 },
+                            shadowColor: "black", shadowOpacity: 0.2, elevation: 1,
+                            marginTop: Platform.OS == "android" ? 30 : null
+                        }}>
+                            <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
+                            <TextInput placeholder="Enter here" placeholderTextColor="grey"
+                                underlineColorAndroid="transparent"
+                                style={{
+                                    flex: 1, fontWeight: "700",
+                                    backgroundColor: "white"
+                                }} />
+
+                        </View>
+
+                    </View>
+                </View>
+            </SafeAreaView>
         )
     }
 }
