3f8b9fe47ec776a59d16e6265da86df44d6d45fa
nonsodaniel <nonsodaniel07@gmail.com>
2020-02-07 12:42:47 +0100

    Added icons and styles for navtabs

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { createBottomTabNavigator } from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
+
 import Explore from './screens/Explore'
 import Inbox from './screens/Inbox'
 import Saved from './screens/Saved'
 import Trips from './screens/Trips'
+import Profile from './screens/Profile'
 
 class App extends Component {
   render() {
@@ -21,18 +24,68 @@ class App extends Component {
 //bottom navigator has 4 screens
 export default createBottomTabNavigator({
   Expolore: {
-    screen: Explore
+    screen: Explore,
+    navigationOptions: {
+      tabBarLabel: "EXPOLORE",
+      tabBarIcon: ({ tintcolor }) => (
+        <Icon name="ios-search" color={tintcolor} size={24} />
+      )
+    }
   },
   Saved: {
-    screen: Saved
+    screen: Saved,
+    navigationOptions: {
+      tabBarLabel: "SAVED",
+      tabBarIcon: ({ tintcolor }) => (
+        <Icon name="ios-heart" color={tintcolor} size={24} />
+      )
+    }
   },
   Trips: {
-    screen: Trips
+    screen: Trips,
+    navigationOptions: {
+      tabBarLabel: "TRIPS",
+      tabBarIcon: ({ tintcolor }) => (
+        <Icon name="ios-jet" color={tintcolor} size={24} />
+      )
+    }
   },
   Inbox: {
-    screen: Inbox
+    screen: Inbox,
+    navigationOptions: {
+      tabBarLabel: "INBOX",
+      tabBarIcon: ({ tintcolor }) => (
+        <Icon name="ios-chatboxes" color={tintcolor} size={24} />
+      )
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: "PROFILE",
+      tabBarIcon: ({ tintcolor }) => (
+        <Icon name="ios-person" color={tintcolor} size={24} />
+      )
+    }
   }
-})
+}, {
+  tabBarOptions: {
+    activeTintColor: "red",
+    inactiveTintColor: "grey",
+    style: {
+      backgroundColor: "white",
+      borderTopWidth: 0,
+      shadowOffSet: { width: 5, height: 3 },
+      shadowColor: "black",
+      shadowOpacity: 0.5,
+      elevation: 5
+    }
+  }
+}
+)
+
+
+
 
 const styles = StyleSheet.create({
   container: {
